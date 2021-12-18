76269d130008e3743982300b3c30ad7729dfb44a
Tushar Sethi <sethitushar2000@gmail.com>
2021-12-18 16:35:36 +0530

    debug true

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--&0m=pqkx=o035ty*!b+b3dqgm^%=6#s#^=xf40i6h^9+0b9)='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Email Purpose
 
